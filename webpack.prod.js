622e189ee7f9dbf602cd6a324b07c4e12b1cc861
alwifdd <alwifdd@gmail.com>
2025-06-02 19:13:45 +0700

    Finalize webpack production config for storyverse-awi deployment

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,20 +1,16 @@
 // webpack.prod.js
-const path = require("path"); // Diperlukan untuk path.resolve jika Anda mendefinisikan ulang output.path
+const path = require("path");
 const common = require("./webpack.common.js");
 const { merge } = require("webpack-merge");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin"); // Anda sudah punya ini
-const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // Anda sudah punya ini
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = merge(common, {
   mode: "production",
   output: {
-    // Nama file JavaScript bundle dengan contenthash untuk caching
     filename: "static/js/[name].[contenthash].bundle.js",
-    // Path di mana aset akan disajikan di server.
-    // Untuk GitHub Pages: '/<NAMA-REPOSITORY>/'
-    publicPath: "/storyverse-pwa/",
-    // path: path.resolve(__dirname, 'dist'), // Ini sudah ada di webpack.common.js, jadi tidak perlu diulang kecuali ingin override total
-    // clean: true, // Bisa juga di sini jika tidak di common, atau gunakan CleanWebpackPlugin
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/storyverse-awi/",
   },
   module: {
     rules: [
@@ -37,16 +33,14 @@ module.exports = merge(common, {
     ],
   },
   plugins: [
-    new CleanWebpackPlugin(), // Membersihkan folder 'dist' sebelum build
+    new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
-      // Nama file CSS output dengan contenthash
       filename: "static/css/[name].[contenthash].css",
     }),
   ],
-  // Anda bisa menambahkan optimization di sini nanti jika perlu
-  // optimization: {
-  //   splitChunks: {
-  //     chunks: 'all',
-  //   },
-  // },
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+    },
+  },
 });
