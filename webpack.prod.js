a8753c19a25a297bb3a5c37d204db6f76d25160e
alwifdd <alwifdd@gmail.com>
2025-06-02 17:26:39 +0700

    Configure for GitHub Pages deployment

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,27 +1,35 @@
-const common = require('./webpack.common.js');
-const { merge } = require('webpack-merge');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// webpack.prod.js
+const path = require("path"); // Diperlukan untuk path.resolve jika Anda mendefinisikan ulang output.path
+const common = require("./webpack.common.js");
+const { merge } = require("webpack-merge");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin"); // Anda sudah punya ini
+const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // Anda sudah punya ini
 
 module.exports = merge(common, {
-  mode: 'production',
+  mode: "production",
+  output: {
+    // Nama file JavaScript bundle dengan contenthash untuk caching
+    filename: "static/js/[name].[contenthash].bundle.js",
+    // Path di mana aset akan disajikan di server.
+    // Untuk GitHub Pages: '/<NAMA-REPOSITORY>/'
+    publicPath: "/storyverse-pwa/",
+    // path: path.resolve(__dirname, 'dist'), // Ini sudah ada di webpack.common.js, jadi tidak perlu diulang kecuali ingin override total
+    // clean: true, // Bisa juga di sini jika tidak di common, atau gunakan CleanWebpackPlugin
+  },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-        ],
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: [
           {
-            loader: 'babel-loader',
+            loader: "babel-loader",
             options: {
-              presets: ['@babel/preset-env'],
+              presets: ["@babel/preset-env"],
             },
           },
         ],
@@ -29,7 +37,16 @@ module.exports = merge(common, {
     ],
   },
   plugins: [
-    new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin(),
+    new CleanWebpackPlugin(), // Membersihkan folder 'dist' sebelum build
+    new MiniCssExtractPlugin({
+      // Nama file CSS output dengan contenthash
+      filename: "static/css/[name].[contenthash].css",
+    }),
   ],
+  // Anda bisa menambahkan optimization di sini nanti jika perlu
+  // optimization: {
+  //   splitChunks: {
+  //     chunks: 'all',
+  //   },
+  // },
 });
